d57e569e837647cf509473a139ba770f2571985b
Jet Pham <jetthomaspham@gmail.com>
2021-09-08 09:44:50 -0700

    Added live calibration to the shooter

--- src/main/java/frc/robot/subsystems/ShooterHood.java
+++ src/main/java/frc/robot/subsystems/ShooterHood.java
@@ -5,14 +5,18 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ShooterHood extends SubsystemBase {
   private final Servo[] ShooterHoodServos = { new Servo(Constants.LeftShooterHoodServo),
       new Servo(Constants.RightShooterHoodServo) };
+  private double SmartDashboardValue;
 
   public ShooterHood() {
+    initShuffleboard();
   }
 
   public void setHoodAngle(double degrees) {
@@ -30,13 +34,31 @@ public class ShooterHood extends SubsystemBase {
     }
   }
 
+  public void setHoodAngleToSmartDashboardValue() {
+    ShooterHoodServos[0].setAngle(SmartDashboardValue);
+    ShooterHoodServos[1].setAngle(Constants.maxHoodValue - SmartDashboardValue);
+  }
+
   public double getHoodAngle() {
     return ShooterHoodServos[0].getAngle();
   }
 
+  private void initShuffleboard() {
+    SmartDashboardTab.putNumber("ShooterHood", "HoodAngle", getHoodAngle());
+  }
+
+  private void updateShuffleboard() {
+    SmartDashboard.putNumber("HoodAngle", getHoodAngle());
+  }
+
+  public void updateShooterAngles() {
+    SmartDashboardValue = SmartDashboardTab.getNumber("ShooterHood", "HoodAngle", -1);
+  }
+
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    updateShuffleboard();
+    updateShooterAngles();
   }
 
   @Override
