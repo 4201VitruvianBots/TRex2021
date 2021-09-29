120ec13561947f0ee60441141ef244f7657dff9f
Josiah Fu <josiah_fu@student.davincischools.org>
2021-09-28 19:51:59 -0700

    Attempted Auto Fixes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -251,7 +251,8 @@ private SkillsChallengeSelector selectedSkillsChallenge = SkillsChallengeSelecto
 //        return new WaitCommand(0);
 //      return new TestAuto(m_swerveDrive, m_FieldSim);
 //    return new AutoNavBarrel(m_swerveDrive, m_FieldSim);
-      return new AutoNavSlalom(m_swerveDrive, m_FieldSim);
+      return new DriveStraight(m_swerveDrive);
+      // return new AutoNavSlalom(m_swerveDrive, m_FieldSim);
 //    return new AutoNavBounce(m_swerveDrive, m_FieldSim);
 //    return new DriveForwardDistance(m_swerveDrive, m_FieldSim, 5);
   }
--- src/main/java/frc/robot/commands/autoCommands/DriveStraight.java
+++ src/main/java/frc/robot/commands/autoCommands/DriveStraight.java
@@ -31,8 +31,8 @@ public class DriveStraight extends SequentialCommandGroup {
                 // Start at the origin facing the +X direction
                 new Pose2d(0, 0, new Rotation2d(0)),
                 List.of(
-                        new Translation2d(1, 0),
-                        new Translation2d(2, 0)
+                        // new Translation2d(1, 0),
+                        // new Translation2d(2, 0)
                 ),
                 // End 3 meters straight ahead of where we started, facing forward
                 new Pose2d(3, 0, new Rotation2d(0)),
